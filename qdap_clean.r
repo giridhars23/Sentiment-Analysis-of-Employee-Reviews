ba1981b9d2ee946c85b57a0718a5ba4d95d7f7e9
Giridhar <giridhars23@gmail.com>
2017-12-05 22:47:49 +0530

    This file contains the function qdap.clean
    qdap.clean is a custom function. It has cleaning functions from qdap libarary to clean raw text.

---
+++ qdap_clean.r
@@ -0,0 +1,9 @@
+qdap_clean <- function(x) {
+  x <- replace_abbreviation(x)
+  x <- replace_contraction(x)
+  x <- replace_number(x)
+  x <- replace_ordinal(x)
+  x <- replace_symbol(x)
+  x <- tolower(x)
+  return(x)
+}
